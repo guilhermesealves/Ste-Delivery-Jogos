cecd8462831aac4b9e434db3b6a1f86f5cd56649
Guilherme Seleri <110575996+guilhermesealves@users.noreply.github.com>
2023-04-27 22:22:00 -0300

    Colocando cores nas variáveis dos tipos de jogos

--- index.php
+++ index.php
@@ -33,38 +33,62 @@
         </button>
     </div>
 
-   <div class="row">
-    <?php
-    $servidor_bd = "127.0.0.1";
-    $usuario_bd = "root";
-    $senha_bd = "";
-    $banco_de_dados = "delivery_jogos_novo";
+    <div class="container mt-4">
+        <div class="row">
+            <?php
+            $servidor_bd = "127.0.0.1";
+            $usuario_bd = "root";
+            $senha_bd = "";
+            $banco_de_dados = "delivery_jogos_novo";
 
-    $conexao = mysqli_connect($servidor_bd, $usuario_bd, $senha_bd, $banco_de_dados);
+            $conexao = mysqli_connect($servidor_bd, $usuario_bd, $senha_bd, $banco_de_dados);
 
-    $sql_buscar = "select * from jogo";
+            $sql_buscar = "select * from jogo";
 
-    $todo_jogos = mysqli_query($conexao, $sql_buscar);
-
-    while ($um_jogo = mysqli_fetch_assoc($todo_jogos)) :
-    ?>
-        <div class="col-md-3 text-center mb-4">
-                <img src="<?php echo $um_jogo["foto"];?>" class = "img-fluid">
-                <h6 class="mt-3 mb-3"><?php echo $um_jogo["categoria"]?></h6>
-                <h5 class="mt-3 mb-3"><?php echo $um_jogo["titulo"];?></h5>
-                <a href="<?php echo $um_jogo["video"];?>"class="btn btn-outline-primary">Ver mais</a>
-            </div>
-        <?php
-    endwhile;
-        ?>
-   </div> 
+            $todo_jogos = mysqli_query($conexao, $sql_buscar);
 
 
 
+            while ($um_jogo = mysqli_fetch_assoc($todo_jogos)) :
+            ?>
+                <div class="col-md-3 text-center mb-4">
+                    <img src="<?php echo $um_jogo["foto"]; ?>" class="img-fluid" style="object-fit: cover; height = 150px;">
+                    <?php
+
+                    $cor = "";
+
+
+                    $categoria = $um_jogo["categoria"];
+
+                    if (strtoupper($categoria == "FPS")) {
+                        $cor = "red";
+                    }else if (strtoupper($categoria == "Ação")){
+                        $cor ="blue";
+
+                    }else if (strtoupper($categoria == "Battle-Royal")){
+                        $cor = "green";
+                    }
+
+                    ?>
+                    <h7 class="mt-3 mb-3" style="color: <?php  echo $cor;  ?>"><?php echo $um_jogo["categoria"] ?></h7>
+                    <h5 class="mt-3 mb-3"><?php echo $um_jogo["titulo"]; ?></h5>
+                    <a href="<?php echo $um_jogo["video"]; ?>" class="btn btn-outline-primary">Ver mais</a>
+                </div>
+            <?php
+            endwhile;
+            ?>
+        </div>
+
+    </div>
 
 
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
